88cdb518265628c20ef8f910489667d7003785ce
socaseinpoint <socaseinpoint@gmail.com>
2018-10-05 20:21:55 +0400

    js

---
+++ tanz/resources/views/intro.blade.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<!-- saved from url=(0061)http://tz.croocky.com/acitdxb.com/tanzanians/status.aspx.html -->
+<html lang="en" id="ls-global"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+<meta name="description" content="QUOTE - Request a quote for every type of companies">
+<meta name="author" content="Ansonika">
+<title>Tanzanians Abroad</title>
+<link rel="shortcut icon" href="http://acitdxb.com/tanzanians/img/favicon1.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="http://acitdxb.com/tanzanians/img/apple-touch-icon-57x57-precomposed1.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="http://acitdxb.com/tanzanians/img/apple-touch-icon-72x72-precomposed1.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="http://acitdxb.com/tanzanians/img/apple-touch-icon-114x114-precomposed1.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="http://acitdxb.com/tanzanians/img/apple-touch-icon-144x144-precomposed1.png">
+
+    <link rel="stylesheet" href="./Tanzanians Abroad_files/font-awesome.min.css">
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="./Tanzanians Abroad_files/css" rel="stylesheet">
+
+    <!-- BASE CSS -->
+	<link href="./Tanzanians Abroad_files/layerslider.min.css" rel="stylesheet">
+    <link href="./Tanzanians Abroad_files/animate.min.css" rel="stylesheet">
+    <link href="./Tanzanians Abroad_files/bootstrap.min.css" rel="stylesheet">
+    <link href="./Tanzanians Abroad_files/style.css" rel="stylesheet">
+    <link href="./Tanzanians Abroad_files/all_icons_min.css" rel="stylesheet">
+    <link href="./Tanzanians Abroad_files/magnific-popup.min.css" rel="stylesheet">
+    <link href="./Tanzanians Abroad_files/yellow.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="./Tanzanians Abroad_files/custom.css" rel="stylesheet">
+
+<style></style><style type="text/css">.backpack.dropzone {
+  font-family: 'SF UI Display', 'Segoe UI';
+  font-size: 15px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  height: 150px;
+  font-weight: lighter;
+  color: white;
+  will-change: right;
+  z-index: 2147483647;
+  bottom: 20%;
+  background: #333;
+  position: fixed;
+  user-select: none;
+  transition: left .5s, right .5s;
+  right: 0px; }
+  .backpack.dropzone .animation {
+    height: 80px;
+    width: 250px;
+    background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/hoverstate.png") left center; }
+  .backpack.dropzone .title::before {
+    content: 'Save to'; }
+  .backpack.dropzone.closed {
+    right: -250px; }
+  .backpack.dropzone.hover .animation {
+    animation: sxt-play-anim-hover 0.91s steps(21);
+    animation-fill-mode: forwards;
+    background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/hoverstate.png") left center; }
+
+@keyframes sxt-play-anim-hover {
+  from {
+    background-position: 0px; }
+  to {
+    background-position: -5250px; } }
+  .backpack.dropzone.saving .title::before {
+    content: 'Saving to'; }
+  .backpack.dropzone.saving .animation {
+    background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/saving_loop.png") left center;
+    animation: sxt-play-anim-saving steps(59) 2.46s infinite; }
+
+@keyframes sxt-play-anim-saving {
+  100% {
+    background-position: -14750px; } }
+  .backpack.dropzone.saved .title::before {
+    content: 'Saved to'; }
+  .backpack.dropzone.saved .animation {
+    background: url("chrome-extension://lifbcibllhkdhoafpjfnlhfpfgnpldfl/assets/backpack/dropzone/saved.png") left center;
+    animation: sxt-play-anim-saved steps(20) 0.83s forwards; }
+
+@keyframes sxt-play-anim-saved {
+  100% {
+    background-position: -5000px; } }
+</style><link rel="stylesheet" href="./Tanzanians Abroad_files/skin.css" type="text/css"><script type="text/javascript" charset="UTF-8" src="./Tanzanians Abroad_files/common.js.download"></script><script type="text/javascript" charset="UTF-8" src="./Tanzanians Abroad_files/util.js.download"></script><script type="text/javascript" charset="UTF-8" src="./Tanzanians Abroad_files/stats.js.download"></script></head>
+
+<body><div class="ls-wp-container fitvidsignore ls-hidden fullsize ls-v6 ls-container ls-fullsize" data-layerslider-uid="LSge8y3p6n8"><div class="ls-slide" data-ls="duration: 8000; transition2d: 5;bgsize:cover;bgposition:10% 15%;kenburnszoom:in;" style="width: 1200px; height: 700px;">
+     			 
+        </div></div>
+<form method="post" action="http://tz.croocky.com/acitdxb.com/tanzanians/status.aspx.html" id="form1" class="ls-direction-fix ls-overflow-visible">
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="48yRNqCBljBsTE8yZZg2CRyVEgLJ3t0DeXahmacnOlEEpoHQQ5920HbLru6ig5vLGVL0K4OOYWDpCGxG2RQRL674kZQjMLBa6HlAR4KUgc30DkJrt/VMKqx33gyuI8UYbL/HCkkVxqsAN3qt4SHRxSO/c1E=">
+
+<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="24F38719">
+    <div id="loader_form">
+		<div data-loader="circle-side-2"></div>
+	</div> 
+  
+    <div id="layerslider" class="fullsize ls-v6 ls-container ls-fullsize" style="width: 1349px; height: 608px; overflow: hidden; margin-left: 0px; margin-right: auto; position: relative; max-width: none; visibility: visible;" data-layerslider-uid="LSge8y3p6n8"> 
+    <!-- First slide -->
+         
+         <header>
+         <div style="position: fixed; z-index:10000000; width:100%; padding:50px; "> 
+        <a id="menu-button-mobile" class="cmn-toggle-switch cmn-toggle-switch__htx"><span>Menu mobile</span></a>
+        <nav class="main_nav">
+            <ul class="nav nav-tabs"> 
+<li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/status.aspx.html">Dashboard</a></li>
+<li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/about.aspx.html">About Us</a></li>
+<li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/services.aspx.html">Services</a></li>
+<li><a href="http://www.immigration.go.tz/index.php/en/download/forms" target="_blank">Citizenship form</a></li>
+<li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/status.aspx.html">Status</a></li>
+<li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/status.aspx.html">Faq</a></li>
+
+
+
+
+
+              <!--  <li><a href="#tab_1" data-toggle="tab">Request a quote</a></li>
+                <li><a href="#tab_2" data-toggle="tab">About</a></li>
+                <li><a href="#tab_3" data-toggle="tab">Contact</a></li> --->
+            </ul>
+        </nav>
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         </div>
+       
+    </header><!-- /header -->
+        
+        
+        
+        
+        
+   
+     <div style="position: absolute; top:33%;  z-index:1000; width:100%; text-align:center;">
+         
+         
+         
+         <div style="font-size:52px; color:#fff;top:55%; line-height:1;text-shadow: 2px 2px 10px rgba(0, 0, 0, 0.60); padding-left:0px; letter-spacing:-1px; margin-bottom:30px;" class="ls-l sliderleft" data-ls="offsetxin:-100;durationin:2000;delayin:1500;offsetxout:-100;durationout:1000;"><img src="./Tanzanians Abroad_files/logo.png"></div>
+
+       
+          
+          
+          <div id="login_div" class="form animated bounceIn">
+               <div style="width:500px; margin:10px auto;"> 
+          		 
+                        
+                   </div>
+  <div style="background-color:#666; width:500px; margin:10px auto;"> 
+        
+    <input name="txtemail" type="text" id="txtemail" placeholder="Email" style="padding:6px 10px; margin:10px 0; border:0;">
+       
+        
+           
+    <input name="txtpassword" type="password" id="txtpassword" placeholder="Password" style="padding:6px 10px; margin:10px 0; border:0;">
+      
+    <button onclick="if (typeof(Page_ClientValidate) == &#39;function&#39;) Page_ClientValidate(&#39;bls1&#39;); __doPostBack(&#39;btnsubscribe&#39;,&#39;&#39;)" id="btnsubscribe" style="background-color:#2db34b; padding:7px 30px; border:0; margin:10px 0; color:#fff;" validationgroup="bls1">Login</button>
+  </div>
+  
+  
+  <a href="http://tz.croocky.com/acitdxb.com/tanzanians/Registration.aspx.html" class="animated infinite pulse" style=" background:transparent; border:0; padding:10px;px 30px; margin:5px; border:0; color:#fff;">Register</a>
+  
+  
+  
+</div>
+
+          
+
+
+
+      </div><!-- /bottom-wizard -->
+        
+       
+		
+	<div class="ls-inner" style="background: none transparent;"><div class="ls-background-videos"></div><div class="ls-slide-backgrounds"><div class="ls-wrapper ls-bg-outer" style="z-index: 1; width: 1349px; height: 608px;"><div class="ls-wrapper ls-bg-wrap" style="background-color: transparent; display: block; opacity: 1; transform: matrix(1.2, 0, 0, 1.2, 0, 0);"><img src="./Tanzanians Abroad_files/slide_1.jpg" class="ls-bg" alt="Slide background" style="margin: 0px; z-index: auto; width: 1349px; height: 759px; padding: 0px; border-width: 0px; border-radius: 0px; x: 0px; y: -22.65px; transform: translateX(0px) translateY(-22.65px); filter: none;" data-slidein="1" data-slideout="1"></div></div></div><div class="ls-layers"></div></div><div class="ls-gui-element ls-bar-timer" style="width: calc(100% - 0px);"></div><div class="ls-gui-element ls-loading-container" style="display: none;"><div class="ls-loading-indicator"></div></div><div class="ls-gui-element ls-shadow" data-parallax="disabled"></div></div><!-- /layerslider -->    
+         
+		
+	
+  
+
+   
+       
+       
+        
+
+    <div id="additional_links" style=" position:absolute; bottom:20px; z-index:10000;">
+        <ul>
+            <li>© 2018 Tanznians Abroad</li>	
+            <li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/disclaimer.aspx.html" class="animated_link">Disclaimer</a></li>
+            <li><a href="http://tz.croocky.com/acitdxb.com/tanzanians/term_and_conditions.aspx.html" class="animated_link">Term and conditions</a></li>
+            <li><a href="http://acitdxb.com/tanzanians/contact.aspx" class="animated_link">Contact Us</a></li>
+           
+        </ul>
+    </div><!-- /add links -->
+    
+    <!-- SCRIPTS -->
+    
+    
+    <style>
+    	#header_in, .main_nav {
+    background-color: transparent;
+}
+.main_nav .nav-tabs>li>a {
+    color: #fff;
+    line-height: 1;
+    padding: 10px 15px;
+    display: inline-block;
+}
+    </style>
+    
+    
+    <!-- Jquery-->
+    <script data-cfasync="false" src="./Tanzanians Abroad_files/email-decode.min.js.download"></script><script src="./Tanzanians Abroad_files/jquery-3.2.1.min.js.download"></script>
+    <!-- Layer slider -->
+	<script src="./Tanzanians Abroad_files/greensock.js.download"></script>
+	<script src="./Tanzanians Abroad_files/layerslider.transitions.js.download"></script>
+	<script src="./Tanzanians Abroad_files/layerslider.kreaturamedia.jquery.js.download"></script>
+    <script src="./Tanzanians Abroad_files/slider_func.js.download"></script>
+    <!-- Common script -->
+    <script src="./Tanzanians Abroad_files/common_scripts_min.js.download"></script>
+    <!-- Theme script -->
+    <script src="./Tanzanians Abroad_files/functions.js.download"></script>
+    <!-- Google map -->
+    <script src="./Tanzanians Abroad_files/js"></script>
+    <script src="./Tanzanians Abroad_files/mapmarker.jquery.js.download"></script>
+    <script src="./Tanzanians Abroad_files/mapmarker_func.jquery.js.download"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function(){
+    //Handles menu drop down
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+    });
+});
+    </script>
+    </form>
+
+    <script type="text/javascript">
+
+               function vali_login(source, args) {
+
+                   if (document.getElementById(source.controltovalidate).value.length > 0) {
+                       args.IsValid = true;
+                       document.getElementById(source.controltovalidate).className = "";
+                   }
+                   else {
+                       args.IsValid = false;
+                       document.getElementById(source.controltovalidate).className = "ErrorControl";
+
+                   }
+
+               }
+               function vali_email(source, args) {
+
+                   if (document.getElementById(source.controltovalidate).value != "") {
+                       if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById(source.controltovalidate).value)) {
+                           args.IsValid = true;
+                           document.getElementById(source.controltovalidate).className = "";
+                       }
+                       else {
+                           args.IsValid = false;
+                            document.getElementById(source.controltovalidate).className = "ErrorControl";
+                       }
+                   } 
+               }
+               function confirm_login(source, args) { 
+                   if (document.getElementById(source.controltovalidate).value == document.getElementById("ContentPlaceHolder1_txtnewpassword").value) {
+                       args.IsValid = true;
+                       document.getElementById(source.controltovalidate).className = "";
+                   }
+                   else {
+                       args.IsValid = false;
+                       document.getElementById(source.controltovalidate).className = "ErrorControl";
+                   }
+
+               }
+               function vali_pass(source, args) {
+
+
+                   if (document.getElementById(source.controltovalidate).value == document.getElementById("ContentPlaceHolder1_txtnewpassword").value) {
+                       args.IsValid = true;
+                       document.getElementById(source.controltovalidate).className = "";
+                   }
+                   else {
+                       args.IsValid = false;
+                       document.getElementById(source.controltovalidate).className = "ErrorControl";
+                   }
+
+               }
+    </script>
+</body></html>
--- tanz/routes/web.php
+++ tanz/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/intro', function () {
+    return view('intro');
+});
