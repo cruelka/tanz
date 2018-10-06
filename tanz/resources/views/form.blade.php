857bf39658b16e5fbe9602336785f5571ba63e89
cruelka <kes1993@ya.ru>
2018-10-06 21:35:02 +0400

    auth

--- tanz/resources/views/form.blade.php
+++ tanz/resources/views/form.blade.php
@@ -1,2 +1,3 @@
-<img  src = 'http://tanzaniansabroad.com/logo.png'>
+<div style = 'background-color: #333; height: auto'><img  src = 'http://tanzaniansabroad.com/logo.png'>
+</div>
 <div>image test</div>
