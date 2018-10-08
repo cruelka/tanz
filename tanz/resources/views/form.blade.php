267dec7fb714515254dfe88e5c26457ad8bc270e
socaseinpoint <socaseinpoint@gmail.com>
2018-10-08 08:31:18 +0400

    js

--- tanz/resources/views/form.blade.php
+++ tanz/resources/views/form.blade.php
@@ -1,3 +1,33 @@
-<div style = 'background-color: #333; height: auto'><img  src = 'http://tanzaniansabroad.com/logo.png'>
+<!--
+<div style = 'background-color: #333; height: auto'>
+    <img  src = 'http://tanzaniansabroad.com/logo.png'>
 </div>
-<div>image test</div>
+-->
+
+<p>THE UNITED REPUBLIC OF TANZANIA</p>
+
+<p>The Third Schedule</p>
+
+<p>DECLARATION CONCERNING CITIZENSHIP</p>
+
+<p>(Under section 6 of the Constitution)</p>
+
+<p>I, {{name}} do solemnly and sincerely declare:</p>
+
+<p>1. That I was born at {{cityname}} in *Tanzania {{cityname}} and am of over the age of 21 years. </p>
+
+<p>2. That I am a citizen of Tanzania by *birth/descent/registration/naturalization and am also a national of {nationcountry} by *birth/descent/registration/naturalization/marriage.</p>
+
+<p>3. That as I am desirous of retaining my status as a citizen of Tanzania, I hereby renounce so far as it lies within my power my status as a *citizen/national of {nameofcounry} and any claim I have to the protection of that country. </p>
+
+<p>Declared at {data} this {data} day of {data} 200 {data}, before me. </p>
+
+<p>*Magistrate/Commissioner for Oaths </p>
+
+<p>*Delete in applicable.</p>
+
+<!--
+<p>OR</p>
+
+<p>(Under sections 3,4 and 7 of the Ordinance) </p>
+-->
