040acd23af493e180f1460089b28a2287bd72379
socaseinpoint <socaseinpoint@gmail.com>
2018-11-08 16:04:00 +0400

    js

--- tanz/resources/views/home.blade.php
+++ tanz/resources/views/home.blade.php
@@ -1,51 +1,57 @@
-@extends('layouts.tanz')
+@extends('layouts.app')
 
 @section('content')
-<!--
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+  <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('{{ asset('./img/bg.jpg') }}');">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+          <div class="brand">
+            <h1>Tanzanians abroad</h1>
+            <h3>Applying for self</h3>
+          </div>
         </div>
+      </div>
     </div>
-</div>
--->
+  </div>
 
-<div class="row">
-                                                            
-                                           
-                                                            <div class="col-lg-3">
-                                                                        <a href="/application"><img src="{{ asset('apply_self.png') }}" class="imageslinks"></a>
-                                                                     </div>
-                                                            <div class="col-lg-3">
-                                                                         <a href="/application"><img class="imageslinks" src="{{ asset('apply_someone.png') }}"></a>
-                                                                     </div>
-                                                            <div class="col-lg-3">
-                                                                         <a href="/status"><img  class="imageslinks" src="{{ asset('view_application.png') }}"></a>
-                                                                     </div>
-                                                            <div class="col-lg-3">
-                                                                         <a href="/settings"><img class="imageslinks" src="{{ asset('change_password.png') }}"></a>
-                                                                     </div>
-                                                 </div>
+  <div class="main main-raised">
+    <div class="section section-basic">
+        
+      <div class="container">
+            <div class="title">
+                <h2>Applications List</h2>
+            </div>
+            
+            <div class="card card-nav-tabs">
+             
+                    <div class="card-body ">
+                      <div class="tab-content text-center">
+                        <div class="tab-pane active show" id="settings">
+                                <div class="alert alert-success" style="    margin: -15px -31px 20px;
+                                border-radius: 10px 10px 0 0;">
+                                        <div class="container">
+                                          <div class="alert-icon">
+                                            <i class="material-icons">check</i>
+                                          </div>
+                                          <b>Success</b> This application confirmed
+                                        </div>
+                                      </div>
+                          <p>ID 3627911209</p>
+                          <button class="btn">Show details<div class="ripple-container"></div></button>
 
-<style>
-@media screen and (max-width: 768px){
-    .imageslinks {
-        display: block;
-    margin: 0 auto;
-    }
-}
-</style>
-@endsection
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  
+            
+
+            
+            </div>
+
+        
+        
+      </div>
+    </div>
+    @endsection
