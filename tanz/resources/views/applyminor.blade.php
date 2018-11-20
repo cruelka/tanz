817fa0d8e3089e6c81f24cf7c7bd2a5e8175783d
socaseinpoint <socaseinpoint@gmail.com>
2018-11-21 01:49:03 +0400

    first

--- tanz/resources/views/applyminor.blade.php
+++ tanz/resources/views/applyminor.blade.php
@@ -1,1 +1,45 @@
-Test
+<div>FORM B</div>
+<div>THE UNITED REPUBEIC OF TANZANIA</div>
+<img src="asset('img/logominor.png')" alt="">
+<div>APPLICATION FOR NATURALISATION AS A CITIZEN OF TANZANIA OF A MINOR CHILD</div>
+<div>(Under sectJoir 10 of the Tanzania Citizenship Act, 1995)</div>
+<div>PART I</div>
+<div>Application</div>
+<div>(Complete either (a), (b), (c) of this portion of the application and delete whichever sections are inappropriate)</div>
+<div>(a) The Parent who is a Citizen of Tanzania;</div>
+<div>I, ...................................................................................................</div>
+<div>(Full name in block capitals)</div>
+<div>of ...................................................................................................</div>
+<div>(Address in block capitals) </div>
+<div>am a citizen of Tanzania and a parent of .............................................................</div>
+<div>......................................................................................................</div>
+<div>to whom the following particulars relate. 1 hereby apply on behalf of my child for *his/her naturalization as a citizen of Tanzania.</div>
+<div>(b) The Parent who is not a Citizen of Tanzania</div>
+<div>(Complete either(i), (ii) or (iii) as applicable).</div>
+<div>I, ...................................................................................................</div>
+<div>(Full name in block capitals)</div>
+<div>of ...................................................................................................</div>
+<div>(Address in block capitals) </div>
+<div>am a parent of ...................................................................................................</div>
+<div>to whom the following particulars relate and am;-</div>
+<div>a national of ...................................................................................................</div>
+<div>and I hereby apply on behalf of  ...................................................................................................</div>
+<div>for *his/her naturalisation as a citizen of Tanzania.</div>
+<div>(c) The Guardian:</div>
+<div>I, ...................................................................................................</div>
+<div>(Full name in block capitals)</div>
+<div>of ...................................................................................................</div>
+<div>(Address in block capitals) </div>
+<div>am the *natural/appointed guardian of  ...................................................................................................</div>
+<div>......................................................................................................</div>
+<div>to whom the following particulars relate, *who has no living parent/whose parent or parents are living and</div>
+<div>resident in ......................................................................................................</div>
+<div>1 am duly authorized by tlie court or by the minor child's father (or mother in the event of his death) whose</div>
+<div>consent is attached and I hereby apply on behalf of  ................................................................... of</div>
+<div>*his/her naturalisation as a citizen of Tanzania.</div>
+<div>Dated at .......................... this ........................... 20 .............</div>
+<div>..................</div>
+<div>Signature</div>
+<div>*Delete as applicable.</div>
+
+<div style="page-break-after: always"></div>
